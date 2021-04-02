c5dfad969985d3983b82b56cb323a7fa9c24c1ab
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-04-02 17:39:46 +0600

    privte route

--- src/App.js
+++ src/App.js
@@ -11,37 +11,41 @@ import Admin from './components/Admin/Admin';
 import Deals from './components/Deals/Deals';
 import Login from './components/Login/Login';
 import Header from './components/Header/Header';
+import { createContext, useState } from 'react';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext()
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <div>
-       
-    <Router>
-    <Header></Header>
-      <div>
-        <Switch>
-          <Route path="/home">
-            <Home />
-          </Route>
-          <Route path="/orders">
-            <Orders />
-          </Route>
-          <Route path="/addJersey">
-            <Admin />
-          </Route>
-          <Route path="/deals">
-            <Deals />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-    </div>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Header></Header>
+        <div>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <PrivateRoute path="/orders">
+              <Orders />
+            </PrivateRoute>
+            <PrivateRoute path="/addJersey">
+              <Admin />
+            </PrivateRoute>
+            <Route path="/deals">
+              <Deals />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,62 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebase.config';
-
+import { useHistory, useLocation } from 'react-router';
+import { UserContext } from '../../App';
 
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
 }
 
 const Login = () => {
+    const [newUser, setNewUser] = useState(true);
+    const [user, setUser] = useState({
+        isSignIn: false,
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    })
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+
     const provider = new firebase.auth.GoogleAuthProvider();
     const handleSignIn = () => {
         firebase.auth()
             .signInWithPopup(provider)
             .then((result) => {
-                const credential = result.credential;
-
-                const token = credential.accessToken;
-                const user = result.user;
+                const { displayName, email } = result.user;
+                const signedInUser = { name: displayName, email };
+                setLoggedInUser(signedInUser);
+                history.replace(from)
             }).catch((error) => {
-                const errorCode = error.code;
                 const errorMessage = error.message;
-                const email = error.email;
-                const credential = error.credential;
+                console.log(errorMessage)
+            });
+    }
+    if (!newUser && user.email && user.password) {
+        firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+            .then((res) => {
+                const newUserInfo = { ...user };
+                newUserInfo.error = '';
+                newUserInfo.success = true;
+                setUser(newUserInfo)
+            })
+            .catch((error) => {
+                const newUserInfo = { ...user };
+                newUserInfo.error = error.message;
+                newUserInfo.success = false;
+                setUser(newUserInfo)
             });
     }
     return (
         <div className="text-center p-5">
-            <button onClick={handleSignIn} className="btn btn-success ">Continue with Google </button>
+            <button onClick={handleSignIn} className="mb-5 btn btn-outline-success rounded-pill">Continue with Google </button>
         </div>
     );
 };
